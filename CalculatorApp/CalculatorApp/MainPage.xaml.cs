b6ee38a1ccf19f55133cd6a10196f5bc24929ee3
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2022-03-17 18:10:18 +0800

    -creating method for BtnCommon, BtnEqual, BtnClear, BtnX, BtnCommonOperation, BtnPercentage, Calculate - livingsneakyCalculator
    -creating icon at easyappicon
    
    -XAML. StackLayout, Frame, Label. Grid, Grid.RowDefinitions, RowDefinition, Grid.ColumnDefinitions, ColumnDefinition, Button, Grid.Row, Grid.Column, Text, Padding, BackgroundColor, Margin, x:Name, FontSize, FontAttributes, HorizontalOptions, Clicked. Grid.RowSpan

--- CalculatorApp/CalculatorApp/MainPage.xaml.cs
+++ CalculatorApp/CalculatorApp/MainPage.xaml.cs
@@ -14,5 +14,110 @@ namespace CalculatorApp
         {
             InitializeComponent();
         }
+
+        private decimal firstNumber;
+        private string operatorName;
+        private bool isOperatorClicked;
+
+        private void BtnCommon_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (LblResult.Text == "0" || isOperatorClicked)
+            {
+                isOperatorClicked = false;
+                LblResult.Text = button.Text;
+            }
+            else 
+            {
+                LblResult.Text += button.Text;
+            }
+        }
+
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            LblResult.Text = "0";
+            isOperatorClicked=false;
+            firstNumber = 0;
+        }
+
+        private void BtnX_Clicked(object sender, EventArgs e)
+        {
+            string number = LblResult.Text;
+            if (number != "0")
+            {
+                number = number.Remove(number.Length - 1, 1);
+                if (string.IsNullOrEmpty(number))
+                {
+                    LblResult.Text = "0";
+                }
+                else 
+                {
+                    LblResult.Text = number;
+                }
+            }
+        }
+
+        public void BtnCommonOperation_Cliked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            isOperatorClicked = true;
+            operatorName = button.Text;
+            firstNumber = Convert.ToDecimal(LblResult.Text);
+        }
+
+        private async void BtnPercentage_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string number = LblResult.Text;
+                if (number != "0")
+                {
+                    decimal percentageValue = Convert.ToDecimal(number);
+                    string result = (percentageValue/100).ToString("0.##");
+                    LblResult.Text = result;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+
+        private void BtnEqual_Clicked(object sender, EventArgs e)
+        {
+            try 
+            {
+                decimal secondNumber = Convert.ToDecimal(LblResult.Text);
+                string finalResult = Calculate(firstNumber, secondNumber).ToString("0.##");
+                LblResult.Text = finalResult;
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+
+        public decimal Calculate(decimal firstNumber, decimal secondNumber)
+        { 
+            decimal result = 0;
+            if (operatorName == "+")
+            {
+                result = firstNumber + secondNumber;
+            }
+            else if(operatorName == "-")
+            {
+                result = firstNumber - secondNumber;
+            }
+            else if (operatorName == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (operatorName == "/")
+            {
+                result = firstNumber / secondNumber;
+            }
+            return result;
+        }
     }
 }
